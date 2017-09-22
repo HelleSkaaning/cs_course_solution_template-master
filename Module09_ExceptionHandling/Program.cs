79059a0d557c2add63547826e6d0ac1b84aaec16
Helle <hsk@itrelation.dk>
2017-09-22 15:16:53 +0200

    fdsfds

--- Module09_ExceptionHandling/Program.cs
+++ Module09_ExceptionHandling/Program.cs
@@ -4,12 +4,68 @@
     {
         static void Main(string[] args)
         {
+            //Dyr d = new Dyr();
+            //try
+            //{
+            //    Dyr d = null;
+            //    System.Console.WriteLine(d.ToString());
+
+            //}
+            //catch (System.NullReferenceException ex)
+            //{
+            //    System.Console.WriteLine(ex.Message);
+            //}
+            //catch (System.Exception ex)
+            //{
+            //    // log
+            //    //redeirect
+            //    System.Console.WriteLine(ex.Message);
+            //}
+
+            try
+            {
+                // Log
+                // redirect
+                F1();
+
+            }
+            catch (System.Exception ex)
+            {
+
+                System.Console.WriteLine(ex.Message);
+                if (ex.InnerException != null)
+                {
+                    System.Console.WriteLine(ex.InnerException.Message);
+½                }
+            }
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 System.Console.Write("Press any key to continue . . . ");
                 System.Console.ReadKey();
             }
+        }
+
+        static void F1()
+        {
+            F2();
+        }
+
+        static void F2()
+        {
+            F3();
+        }
+
+        static void F3()
+        {
+            Dyr d = null;
+            System.Console.WriteLine(d.ToString());
+
+        }
+
+        class Dyr
+        {
+
         }
     }
 }
