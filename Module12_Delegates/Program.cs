6050024a8dfcc2c9580aabc404998684f172af00
admin <admin@AR-112-05>
2017-10-10 14:33:34 +0200

    Eventhandler

--- Module12_Delegates/Program.cs
+++ Module12_Delegates/Program.cs
@@ -9,10 +9,77 @@ namespace Module12_Delegates
         public delegate double MinDelegate3(double a, double b);
         public delegate void MinDelegate4(string s);
 
+        class Hund
+        {
+            public string Navn { get; set; }
+            public event EventHandler SkalTisse;
+            public event EventHandler<HundeTisEventArgs> SkalTisse2;
+
+            public void Test() {
+                if (SkalTisse != null)
+                {
+                    SkalTisse(this, new EventArgs());
+                    SkalTisse2(this, new HundeTisEventArgs() { Niveau = 80});
+                }
+            }
+                 
+        }
+
+        class HundeTisEventArgs : EventArgs {
+            public int Niveau { get; set; }
+
+        }
+
+
         static void Main(string[] args)
         {
-            MinDelegate m1 = Test1; // new MinDelegate(Test1);
 
+            Hund h = new Hund();
+            h.Navn = "Fido";
+            h.SkalTisse += (s, e) => {
+                Hund hund = s as Hund;
+                Console.WriteLine(hund.Navn +" skal Tisse"); };
+            h.SkalTisse += (s, e) => { Console.WriteLine("Jeg vil UUUDDD "); };
+            h.SkalTisse2 += (s, e) => {
+                Hund hund = s as Hund;
+                Console.WriteLine(hund.Navn + " skal Tisse på niveau "+ e.Niveau);
+            };
+
+            h.Test();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+
+
+            return;
+
+            #region test
+            /*
+            System.Timers.Timer tt = new System.Timers.Timer();
+            tt.Enabled = true;
+            tt.Interval = 1000;
+            tt.Elapsed += (s, e) => { Console.WriteLine("Tick"); };
+
+
+            do { } while (true);
+
+            //Console.ReadKey();
+
+
+            System.IO.FileSystemWatcher fw = new System.IO.FileSystemWatcher(@"c:\temp");
+            fw.EnableRaisingEvents = true;
+
+            fw.Created += Fw_Created;
+            fw.Deleted += delegate (object sender, System.IO.FileSystemEventArgs e) { Console.WriteLine("Fil slettet"); };
+
+            fw.Changed += (s, e) => { Console.WriteLine("Fil ændret"); };
+
+
+
+            MinDelegate m1 = Test1; // new MinDelegate(Test1);
             // kan afvikles på begge måde
             Test1();
             m1.Invoke();
@@ -25,17 +92,49 @@ namespace Module12_Delegates
             res2 = m2(45); // syntakssukker
 
             MinDelegate3 m3 = Test3;
-            double b = m3(3,6);
+            double g = m3(3,6);
 
             MinDelegate4 m4 = Console.WriteLine;
             m4 += Test4;
             m4("Hfjkld");
-            
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                System.Console.Write("Press any key to continue . . . ");
-                System.Console.ReadKey();
-            }
+
+            Action p1 = Test1;
+            Action<string> p2 = Console.WriteLine;
+            Action<string, object> p3 = Console.Write;
+
+            Func<int, int, int> p4 = Test5;
+            Func<double, double, double> p5 = Math.Atan2; // Find noget, der passer
+
+            Action<string> p6 = Test4;
+            p6 += Console.WriteLine;
+            p6 += delegate (string txt) { Console.WriteLine("I min anonyme funktion" + txt); };
+            p6 += delegate (string t) { System.IO.File.WriteAllText(@"c:\temp\t.txt", t); };
+            p6 += (string t) => { System.IO.File.WriteAllText(@"c:\temp\t2.txt", t); };
+            p6 += t => { System.IO.File.WriteAllText(@"c:\temp\t3.txt", t); };
+            p6("ffgg");
+
+            Action<string, string> p7 = (p, c) => { System.IO.File.WriteAllText(p, c); };
+            p7(@"c:\temp\t4.txt", "flkgæsjglksgjlkfæjgæls");
+
+
+            int[] a = { 2, 3, 76, 45, 7, 5, 77 };
+            var b = Array.FindAll(a, type => type < 10);
+*/
+
+ #endregion
+        }
+
+
+        class Dice
+        {
+            public int Value { get; set; }
+            // Gør den færdig en anden dag
+        }
+
+
+        private static void Fw_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine("Fil oprettet: "+ e.FullPath);
         }
 
         static double Test3(double a, double b)
@@ -47,6 +146,12 @@ namespace Module12_Delegates
         {
             Console.WriteLine(a);
             return true;
+        }
+
+        static int Test5(int a, int b)
+        {
+            Console.WriteLine(a);
+            return a+b;
         }
 
         static void Test1()
---
+++ Tid/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maskine m = new Maskine();
+            m.Log = Console.WriteLine;
+            //m.Log += AppendLog;
+            //m.Log += delegate (string t) { System.IO.File.AppendAllText(@"c:\temp\helle.txt", t + "\r\n"); Console.WriteLine("Skriver til fil"); };
+            m.Log += t => { System.IO.File.AppendAllText(@"c:\temp\helle.txt", t + "\r\n"); Console.WriteLine("Skrev til fil"); };
+
+            m.StartLog();
+            m.EndLog();
+
+            Console.ReadKey();
+
+        }
+
+        //static void AppendLog(string t)
+        //{
+        //    System.IO.File.AppendAllText(@"c:\temp\helle.txt", t+"\r\n");
+
+        //}
+
+        public delegate void LogDelegate(string t);
+        public class Maskine
+        {
+
+            public LogDelegate Log { get; set; }
+
+            //public void StartLog()
+            //{
+            //    Log(DateTime.Now.ToLongTimeString("N2")+" Starter");
+            //}
+            public void StartLog() => Log(DateTime.Now.ToString("HH:mm:ss") + " Starter");
+
+            //public void EndLog()
+            //{
+            //    Log(DateTime.Now.ToLongTimeString() + " Stopper");
+            //}
+
+            public void EndLog() => Log(DateTime.Now.ToLongTimeString() + " Stopper");
+
+        }
+
+    }
+}
